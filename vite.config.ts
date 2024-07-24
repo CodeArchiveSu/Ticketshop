ce919514df2d3fbd6b6a2f320061a4fb1e43c2a1
kishyn4086 <kishyn4086@gmail.com>
2024-07-24 14:23:52 +0200

    route testing

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: ".",
-  plugins: [react()],
+// export default defineConfig({
+//   base: ".",
+//   plugins: [react()],
+// });
+
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === "production" ? "/Ticketshop/" : "/",
+    plugins: [react()],
+  };
 });
