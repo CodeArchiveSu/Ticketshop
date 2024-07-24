f198970e3c168aa9ed5372c6509c3faca1fbbda9
kishyn4086 <kishyn4086@gmail.com>
2024-07-24 14:09:51 +0200

    base change

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "https://codearchivesu.github.io/Ticketshop/",
-  plugins: [react()],
+// export default defineConfig({
+// base: mode === 'production' ? 'https://codearchivesu.github.io/Ticketshop/' : '/',
+//   plugins: [react()],
+// });
+
+export default defineConfig(({ mode }) => {
+  return {
+    base:
+      mode === "production"
+        ? "https://codearchivesu.github.io/Ticketshop/"
+        : "/",
+    plugins: [react()],
+  };
 });
